5c68a9b8623d45a408b844bd1dca6b9e32837836
Your Name <you@example.com>
2026-02-02 19:12:42 +0200

    update main.go file

--- Backend/main.go
+++ Backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -11,6 +12,7 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]string{
 		"msg": "healthy",
 	})
+	fmt.Println("Health check endpoint hit")
 }
 
 func main() {
@@ -22,5 +24,5 @@ func main() {
 	//     AllowCredentials: true,
 	// })
 	http.HandleFunc("/health", createUser)
-	http.ListenAndServe("localhost:8080", nil)
+	http.ListenAndServe("0.0.0.0:8080", nil)
 }
